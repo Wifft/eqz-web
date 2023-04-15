c19ab6b3d10a836e5093ce50eacceb5a464296e4
Wifft <90070891+Wifft@users.noreply.github.com>
2023-04-15 20:15:14 +0200

    Added dynamic sortable blueprints detection

--- plugins/js/tailorutils/Plugin.php
+++ plugins/js/tailorutils/Plugin.php
@@ -2,14 +2,10 @@
 
 use Backend\Classes\NavigationManager;
 use System\Classes\PluginBase;
+use Yaml;
 
 final class Plugin extends PluginBase
 {
-    public const SORTABLE_BLUEPRINTS = [
-        'OCTOBER.TAILOR.ENTRY_CREATORS_CREATOR',
-        'OCTOBER.TAILOR.ENTRY_ROSTERS_ROSTER'
-    ];
-
     /**
      * @inheritDoc
      */
@@ -34,7 +30,7 @@ final class Plugin extends PluginBase
         /** @var string $key */
         /** @var \Backend\Classes\MainMenuItem $item */
         foreach ($navItems as $key => &$item) {
-            if (in_array($key, self::SORTABLE_BLUEPRINTS)) {
+            if (in_array($key, self::getSortableBlueprints())) {
                 $item->config['url'] = self::replaceMenuUrl($item->config['url']);
 
                 if (isset($item->config['sideMenu'])) {
@@ -48,7 +44,72 @@ final class Plugin extends PluginBase
     }
 
     /**
-     * @inheritDoc
+     * Gets all tailor blueprints that has 'sort_order' field.
+     *
+     * @return array
+     *
+     * @static
+     */
+    private static function getSortableBlueprints() : array
+    {
+        /** @var array */
+        $sortableBlueprints = [];
+
+        /** @var string */
+        $blueprintsPath = __DIR__ . '/../../../app/blueprints';
+
+        /** @var array */
+        $blueprintFolders = array_slice(scandir($blueprintsPath), 3);
+
+        /** @var string */
+        foreach ($blueprintFolders as $folder) {
+            /** @var string */
+            $currentFolder = "$blueprintsPath/$folder";
+            if (is_dir($currentFolder)) {
+                /** @var array */
+                $blueprintEntries = array_slice(scandir($currentFolder), 2);
+
+                /** @var string */
+                foreach ($blueprintEntries as $entry) {
+                    $entryFile = "$currentFolder/$entry";
+                    if (is_file($entryFile)) {
+                        /** @var array */
+                        $entryData = Yaml::parseFile($entryFile);
+                        if (isset($entryData['fields']['sort_order'])) {
+                            $sortableBlueprints[] = self::getNavigationItemKey($entryData['handle']);
+                        }
+                    }
+                }
+            }
+        }
+
+        return $sortableBlueprints;
+    }
+
+    /**
+     * Builds a navigation key for the current sortable item.
+     *
+     * @param string $rawName given raw handle name.
+     *
+     * @return string
+     *
+     * @static
+     */
+    private static function getNavigationItemKey(string $rawName) : string
+    {
+        $blueprintName = str_replace('\\', '_', strtoupper($rawName));
+
+        return "OCTOBER.TAILOR.ENTRY_$blueprintName";
+    }
+
+    /**
+     * Replaces menu item navigation URL.
+     *
+     * @param string $url current URL.
+     *
+     * @return string
+     *
+     * @static
      */
     private static function replaceMenuUrl(string $url) : string
     {
