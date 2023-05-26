b383a676618db9f65553516d7a84898dc4f0001f
Wifft <info@josepsalva.name>
2023-05-26 15:40:05 +0200

    Fixed some bugs

--- app/console/ImportYouTubeVideos.php
+++ app/console/ImportYouTubeVideos.php
@@ -31,16 +31,19 @@ final class ImportYouTubeVideos extends Command
 
         /** @var object $video */
         foreach ($videos as $video) {
-            YouTubeVideo::firstOrCreate(
-                [
-                    'video_id' => $video->id->videoId
-                ],
-                [
-                    'title' => $video->snippet->title,
-                    'video_thumb_url' => $video->snippet->thumbnails->high->url,
-                    'upload_date' => Carbon::parse($video->snippet->publishedAt)->toDateTimeString()
-                ]
-            );
+            $videoInfo = Youtube::getVideoInfo($video->id->videoId);
+            if ($videoInfo->status->privacyStatus === 'public') {
+                YouTubeVideo::firstOrCreate(
+                    [
+                        'video_id' => $video->id->videoId
+                    ],
+                    [
+                        'title' => $video->snippet->title,
+                        'video_thumb_url' => $video->snippet->thumbnails->high->url,
+                        'upload_date' => Carbon::parse($video->snippet->publishedAt)->toDateTimeString()
+                    ]
+                );
+            }
         }
     }
 }
