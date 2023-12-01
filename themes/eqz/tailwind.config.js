9a4063f6037c507fe073d53dc3552a6faa6be167
Wifft <info@josepsalva.name>
2023-12-01 20:45:19 +0100

    Updated brand

--- themes/eqz/tailwind.config.js
+++ themes/eqz/tailwind.config.js
@@ -13,8 +13,8 @@ module.exports = {
             colors: {
                 'primary': '#010519',
                 'primary--translucent': '#010519d9',
-                'secondary': '#44c4fd',
-                'secondary--translucent': '#44c4fdd9',
+                'secondary': '#fcf900',
+                'secondary--translucent': '#fcf900d9',
                 'overlay': 'rgba(88.0,88.0,88.0,0.5)',
                 'overlay--v2': 'rgba(192.0,192.0,192.0,0.7)',
                 'whitesmoke--translucent': '#fdfdfdd9'
